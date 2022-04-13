7394e5e7449d283f9aa7d7e4466ff4738f5962cd
nlueb <9465658+nlueb@users.noreply.github.com>
2022-04-13 18:05:07 +0200

    Initial commit

---
+++ src/rational.test.cpp
@@ -0,0 +1,30 @@
+#include <catch2/catch_test_macros.hpp>
+#include <sstream>
+#include "rational.hpp"
+
+TEST_CASE("rational") {
+    Rational a;
+    Rational b{ 2, 4 };
+    Rational c{ 1, 3 };
+    REQUIRE((a.counter == 1 && a.denominator == 2));
+    REQUIRE((b.counter == 2 && b.denominator == 4));
+    REQUIRE((c.counter == 1 && c.denominator == 3));
+
+    Rational d = a + b;
+    REQUIRE(d.counter == d.denominator);
+    d = a - b;
+    REQUIRE(d.counter == 0);
+    d = a * b;
+    REQUIRE(4 * d.counter == d.denominator);
+    d = a / b;
+    REQUIRE(d.counter == d.denominator);
+
+    d = simplify({ 2, 4 });
+    REQUIRE((d.counter == 1 && d.denominator == 2));
+    d = simplify({ 0, 2 });
+    REQUIRE(d.counter == 0);
+
+    std::ostringstream oss;
+    oss << a;
+    REQUIRE(oss.str() == "1/2");
+}
