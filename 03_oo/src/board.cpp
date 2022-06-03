dcfa33cdc67c93f1f6b3f635b6fe148c6563d10b
nlueb <9465658+nlueb@users.noreply.github.com>
2022-06-03 15:02:09 +0200

    Finish 3.1a)

--- 03_oo/src/board.cpp
+++ 03_oo/src/board.cpp
@@ -2,6 +2,17 @@
 
 #include "player.hpp"
 
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <optional>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+constexpr std::size_t BOARD_SIZE = 3;
+
 GameStatus asGameStatus(Color color)
 {
     switch (color) {
@@ -46,13 +57,43 @@ Color enemyOf(Color color)
 
 Board::Board()
     : fields(std::vector<std::vector<Field>>(
-        3, std::vector<Field>(3, Field::EMPTY)))
+        BOARD_SIZE, std::vector<Field>(BOARD_SIZE, Field::EMPTY)))
 {
 }
 
+const std::vector<std::vector<std::pair<std::size_t, std::size_t>>> win_conditions {
+    { std::make_pair(0, 0), std::make_pair(0, 1), std::make_pair(0, 2) },
+    { std::make_pair(1, 0), std::make_pair(1, 1), std::make_pair(1, 2) },
+    { std::make_pair(2, 0), std::make_pair(2, 1), std::make_pair(2, 2) },
+    { std::make_pair(0, 0), std::make_pair(1, 0), std::make_pair(2, 0) },
+    { std::make_pair(0, 1), std::make_pair(1, 1), std::make_pair(2, 1) },
+    { std::make_pair(0, 2), std::make_pair(1, 2), std::make_pair(2, 2) },
+    { std::make_pair(0, 0), std::make_pair(1, 1), std::make_pair(2, 2) },
+    { std::make_pair(0, 2), std::make_pair(1, 1), std::make_pair(2, 0) },
+};
+
 std::optional<GameStatus> Board::whoWon() const
 {
-    return {}; // TODO
+    for (const auto& win_condition : win_conditions) {
+        if (std::all_of(std::begin(win_condition), std::end(win_condition), [this](const auto& x) {
+                return fields[x.first][x.second] == Field::CROSS;
+            })) {
+            return GameStatus::CROSS;
+        }
+        if (std::all_of(std::begin(win_condition), std::end(win_condition), [this](const auto& x) {
+                return fields[x.first][x.second] == Field::CIRCLE;
+            })) {
+            return GameStatus::CIRCLE;
+        }
+    }
+    if (std::any_of(std::begin(fields), std::end(fields), [](const auto& row) {
+            return std::any_of(std::begin(row), std::end(row), [](Field field) {
+                return field == Field::EMPTY;
+            });
+        })) {
+        return std::nullopt;
+    }
+    return GameStatus::TIE;
 }
 
 std::ostream& operator<<(std::ostream& os, const Board& board)
