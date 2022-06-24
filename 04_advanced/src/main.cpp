44c1ff9e3a83c0f3812733b6062418b7b309ca3d
Robin <robin.mueller@stud.tu-darmstadt.de>
2022-06-24 13:41:11 +0200

    Ass55555

--- 04_advanced/src/main.cpp
+++ 04_advanced/src/main.cpp
@@ -13,5 +13,8 @@
 
 int main(int argc, char** argv)
 {
-    // TODO teste deine Implementationen
+    auto fx = [](double x){ return std::pow(x, 3) + 4 * std::pow(x, 2) - 7 * x + 12; };
+    auto fderiv = [](float x){ return 3 * std::pow(x, 2) + 8 * x - 7; };
+    double solution = newton(fx, fderiv, 0, 1000);
+    std::cout << "4.2 a): " << solution << std::endl;
 }
--- 04_advanced/src/numerik.cpp
+++ 04_advanced/src/numerik.cpp
@@ -4,6 +4,9 @@
 
 double newton(std::function<double(double)> fx, std::function<float(float)> fderiv, double x0, size_t n)
 {
-    return {};
-    // TODO
+    if (n == 0) {
+        return x0;
+    }
+    auto x_prev = newton(fx, fderiv, x0, n - 1);
+    return x_prev - fx(x_prev) / fderiv(x_prev);
 }
